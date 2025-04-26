220d8f636d568c9c06f3c3b368c08c6a62d330c6
Hugo Lonnqvist <h.lonnqvist@icloud.com>
2025-04-26 11:50:01 +0200

    Update Trieste git tag and tidy up opt rewritere

--- src/optimization_analysis.cc
+++ src/optimization_analysis.cc
@@ -5,16 +5,15 @@ namespace whilelang {
 
     Rewriter optimization_analysis(bool run_zero_analysis) {
         auto control_flow = std::make_shared<ControlFlow>();
-        auto control_is_dirty = [=](Node) { return control_flow->is_dirty(); };
+        auto control_is_dirty = [=](Node) { return false; };
+        auto should_run_zero_analysis = [=](Node) { return run_zero_analysis; };
 
         Rewriter rewriter = {
             "optimization_analysis",
             {
                 gather_instructions(control_flow),
                 gather_flow_graph(control_flow),
-                z_analysis(control_flow).cond([=](Node) {
-                    return run_zero_analysis;
-                }),
+                z_analysis(control_flow).cond(should_run_zero_analysis),
                 constant_folding(control_flow),
 
                 gather_instructions(control_flow).cond(control_is_dirty),
